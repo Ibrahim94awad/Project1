f14c85ed375432cdaeaa809a0032ce1fef2a514a
Ibrahim Alawad <ibrahim94awad@gmail.com>
2021-11-02 14:51:20 +0100

    [UPD]set LibraryContext

--- WPFproject1/WPFproject1/MainWindow.xaml.cs
+++ WPFproject1/WPFproject1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibraryLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,7 @@ namespace WPFproject1
     /// </summary>
     public partial class MainWindow : Window
     {
+        LibraryContext
         public MainWindow()
         {
             InitializeComponent();
